8b48ec60b0a7bb2a636986e9719c2265b85e3e97
Fernando256 <luisfernando_paganini@hotmail.com>
2021-10-27 21:39:18 -0300

    update deleted files and framework

--- js/list-page.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $('input[name="daterange"]').daterangepicker({
-      open: 'left'
-    }, function(start, end) {
-      console.log("A new date selection was made: " + start.format('DD-MM-YYYY') + ' to ' + end.format('DD-MM-YYYY'));
-    });
-  });
